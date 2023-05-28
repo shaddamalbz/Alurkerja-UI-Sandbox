55bf92baabee7c3d7355b20eeb2b3f01332389cb
shaddamalbz <shaddam.albz@gmail.com>
2023-05-28 13:50:55 +0700

    update deps & fix admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-
 import { Header, Sidebar } from 'alurkerja-ui'
+import clsx from 'clsx'
 
 export default function AdminLayout() {
   const [toggled, setToggled] = useState(false)
 
   return (
-    <div className="flex">
-      <Sidebar toggled={toggled} setToggled={setToggled} menuConfig={[]} />
-      <div className="grow">
+    <>
+      <div className="fixed h-screen">
+        <Sidebar toggled={toggled} setToggled={setToggled} menuConfig={[]} />
+      </div>
+      <div
+        className={clsx(
+          'transition-[margin] ease-in-out duration-400',
+          toggled ? 'sm:ml-[80px]' : 'sm:ml-[270px]'
+        )}
+      >
         <Header />
-        <main className=" w-full min-h-[calc(100vh-64px)] bg-slate-100 px-4 py-8">
+        <main className="px-4 py-8">
           <Outlet />
         </main>
       </div>
-    </div>
+    </>
   )
 }
